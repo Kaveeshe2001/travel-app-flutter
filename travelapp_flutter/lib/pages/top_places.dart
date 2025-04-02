b405ce37ca7320e77f77fd5655429fb6d714b63f
Kaveesha <kaveeshewaduge111@gmail.com>
2025-04-03 00:52:25 +0530

    complete topplaces page

--- travelapp_flutter/lib/pages/top_places.dart
+++ travelapp_flutter/lib/pages/top_places.dart
@@ -41,7 +41,163 @@ class _TopPlacesState extends State<TopPlaces> {
               ],),
             ),
 
-            
+            SizedBox(height: 20.0,),
+            Expanded(
+              child: Material(
+                elevation: 3.0,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+                child: Container(
+                  padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 20.0),
+                  decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            children: [ 
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                "images/sigiriya.jpg",
+                                height: 220,
+                                width: 140,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 190),
+                              width: 140,
+                              decoration: BoxDecoration(color: Colors.black26, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
+                              child:Center(
+                                child: Text(
+                                  "Sigiriya",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            )
+                            ]
+                          ),
+                        ),
+
+                        Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            children: [ 
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                "images/tooth.jpg",
+                                height: 220,
+                                width: 140,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 190),
+                              width: 140,
+                              decoration: BoxDecoration(color: Colors.black26, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
+                              child:Center(
+                                child: Text(
+                                  "Tooth Te",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            )
+                            ]
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 20.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            children: [ 
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                "images/nuwaraeliya.jpg",
+                                height: 220,
+                                width: 140,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 190),
+                              width: 140,
+                              decoration: BoxDecoration(color: Colors.black26, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
+                              child:Center(
+                                child: Text(
+                                  "Nuwaraeliya",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            )
+                            ]
+                          ),
+                        ),
+
+                        Material(
+                          elevation: 3.0,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            children: [ 
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                "images/horton.jpg",
+                                height: 220,
+                                width: 140,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 190),
+                              width: 140,
+                              decoration: BoxDecoration(color: Colors.black26, borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
+                              child:Center(
+                                child: Text(
+                                  "Horton",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            )
+                            ]
+                          ),
+                        ),
+                      ],
+                    )
+
+                  ],),
+                ),
+              ),
+            )
           ],
         ),
       )
